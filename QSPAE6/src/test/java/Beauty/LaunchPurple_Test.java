6b1ab82b0008ffbd30523a7b893eaac59741dfa1
Dell <Dell@DESKTOP-52INU0K>
2024-03-29 20:29:33 -0700

    I have added a test script(LaunchPurple_Test) inside beauty package

---
+++ QSPAE6/src/test/java/Beauty/LaunchPurple_Test.java
@@ -0,0 +1,13 @@
+package Beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LaunchPurple_Test {
+	
+	@Test
+	public void purple() {
+		Reporter.log("test case" , true);
+	}
+
+}
